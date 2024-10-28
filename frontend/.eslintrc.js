b3c7837ced17ffe7b42a9ccd1befe4cb07903c9e
kalebE36 <kaleb.ellington33@gmail.com>
2024-10-28 15:43:25 -0400

    test linter

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-    extends: ['expo'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-      'no-undef': 'error',                            // Treat undefined variables as an error
-      '@typescript-eslint/no-explicit-any': 'error', // Avoid using 'any' type
-    }
-  };
-  
+  extends: ['expo'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-undef': 'error',                            // Treat undefined variables as an error
+    '@typescript-eslint/no-explicit-any': 'error', // Avoid using 'any' type
+  }
+};
