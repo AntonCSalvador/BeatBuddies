581d87448467b66c6c5f11b04de4eda8bb1c734f
kalebE36 <kaleb.ellington33@gmail.com>
2024-10-28 15:40:35 -0400

    tailwind change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+        './app/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+        './screens/**/*.{js,jsx,ts,tsx}',
+        './utils/**/*.{js,jsx,ts,tsx}',
+        './hooks/**/*.{js,jsx,ts,tsx}',
+        './contexts/**/*.{js,jsx,ts,tsx}',
+      ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
