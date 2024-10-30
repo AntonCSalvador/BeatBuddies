1ca2c8ebb4bdf0753179c41182e4cd2aff53e392
kalebE36 <kaleb.ellington33@gmail.com>
2024-10-30 15:32:27 -0400

    added profile components

---
+++ frontend/components/profile/index/BodyProfIndex.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { signOut } from '@/utils/auth';
+import { Alert } from 'react-native';
+import { useRouter } from "expo-router";
+import { View, Text } from "react-native";
+import LinkOptions from "./LinkOptions";
+
+
+
+
+export default function BodyProfIndex() {
+    const router = useRouter(); 
+
+    const confirmSignOut = () => {
+        Alert.alert(
+            'Confirm Sign Out',
+            'Are you sure you want to sign out?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Yes',
+                    onPress: () => signOut(),
+                },
+            ],
+            { cancelable: true }
+        );
+    };
+
+    return (
+<View className="flex-1 w-full relative">
+            {/* Profile Header */}
+            <View className="w-full h-[7%]">
+                <Text className="text-2xl pt-4 pl-4 font-nunito-extrabold">
+                    Profile
+                </Text>
+            </View>
+
+            {/* Profile Options */}
+            <View className="w-full">
+                <LinkOptions
+                    title="Account Info"
+                    onPress={() => router.push('/(pages)/home')}
+                    color="black"
+                />
+                <LinkOptions
+                    title="Transaction History"
+                    onPress={() =>
+                        router.push('/(pages)/home')
+                    }
+                    color="black"
+                />
+            </View>
+
+            {/* Security Header */}
+            <View className="w-full h-[7%] mt-4">
+                <Text className="text-2xl pt-4 pl-4 font-nunito-extrabold">
+                    Security
+                </Text>
+            </View>
+
+            {/* Security Options */}
+            <View className="w-full">
+                <LinkOptions
+                    title="Sign Out"
+                    onPress={confirmSignOut}
+                    color="red"
+                />
+            </View>
+        </View>
+    );
+}
--- frontend/screens/pages/profile/profilescreen.tsx
+++ frontend/screens/pages/profile/profilescreen.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Button, View } from 'react-native';
 import { signOut } from '@/utils/auth';
+import HeaderProfile from '@/components/profile/HeaderProfile';
 import SafeAreaViewAll from '@/components/general/SafeAreaViewAll';
+import BodyProfIndex from '@/components/profile/index/BodyProfIndex';
 
 export default function ProfileScreen() {
     return (
         <SafeAreaViewAll color="white">
             <View className="flex-1">
-                <Button
-                    title="Signout"
-                    onPress={() => {
-                        signOut();
-                    }}
-                />
+                <HeaderProfile />
+                <BodyProfIndex />
             </View>
         </SafeAreaViewAll>
     );
