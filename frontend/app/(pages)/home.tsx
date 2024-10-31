2dca7dd37e48b3588d36b2bd8b4083eaf0469007
AntonCSalvador <antoncsalvador@gmail.com>
2024-10-31 03:02:45 -0400

    fixed

--- frontend/app/(pages)/home.tsx
+++ frontend/app/(pages)/home.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import HomeScreen from '@/screens/pages/home/homescreen';
+import SafeAreaViewAll from '@/components/general/SafeAreaViewAll';
 
 export default function HomePage() {
-    return <View></View>;
+    return (
+        <SafeAreaViewAll color="white">
+            <HomeScreen />
+        </SafeAreaViewAll>
+    );
 }
