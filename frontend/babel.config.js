3b073023dc25ece7e017f75c4eea972ca4d95e4a
kalebE36 <kaleb.ellington33@gmail.com>
2024-11-13 21:18:35 -0500

    HOTFIX

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,14 +1,22 @@
-module.exports = function (api) {
-  api.cache(true);
+const { getDefaultConfig } = require('expo/metro-config');
 
-  return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
-      'nativewind/babel',
-      ['module:react-native-dotenv', {
-        "moduleName": "@env",
-        "path": ".env",
-      }],
-    ],
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  // Enable CJS extension support for Expo Router
+  config.resolver.sourceExts = [
+    ...config.resolver.sourceExts,
+    'cjs',
+  ];
+
+  // SVG transformer configuration (if applicable)
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   };
-};
+
+  config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+  config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+  return config;
+})();
--- frontend/env.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@env' {
-    export const SPOTIFY_CLIENT_ID: string;
-    export const SPOTIFY_CLIENT_SECRET: string;
-  }
-  
