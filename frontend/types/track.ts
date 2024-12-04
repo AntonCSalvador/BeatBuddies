b31f378a353d8c368d41e582b50b77ac2d2093a1
kalebE36 <kaleb.ellington33@gmail.com>
2024-12-04 01:34:01 -0500

    different tracks

--- frontend/types/track.ts
+++ frontend/types/track.ts
@@ -1,9 +1,9 @@
 export default interface Track {
     id: string;
     name: string;
-    artists: { name: string }[];
+    artists: { name: string }[]; // Array of artist objects
     album: {
         name: string;
         images: { url: string }[];
     };
-}
+};
